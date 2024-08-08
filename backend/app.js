0ee29bcb23646cc435867f967973778d0e7cacea
team1-at-668567942749 <team1-at-668567942749>
2024-08-08 11:42:01 +0530

    updated backend

--- backend/app.js
+++ backend/app.js
@@ -1,48 +1,44 @@
 const express = require('express');
-
-
-const path = require("path")
+const path = require('path');
 const bodyParser = require('body-parser');
-const mongoose = require('mongoose')
-const Post = require('./models/post')
-const authMiddleware = require("./middleware/check.auth");
-const app = express();
+const mongoose = require('mongoose');
 const cors = require('cors');
-app.use("/images",express.static(path.join("backend/images")));
-const postRoute = require("./routes/post");
 
-const userRoute = require("./routes/user")
+const Post = require('./models/post');
+const authMiddleware = require('./middleware/check.auth');
 
+const app = express();
+app.use('/images', express.static(path.join('backend/images')));
 app.use(cors());
 
-mongoose.set('strict', false);
-mongoose.connect("mongodb://127.0.0.1:27017/marketPlace", { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => {
-    console.log('Connected to the database!');
-  })
-  .catch((err) => {
-    console.error('Failed to connect:', err);
-  });
+const postRoute = require('./routes/post');
+const userRoute = require('./routes/user');
+
+const uri = 'mongodb+srv://admin:admin@cluster0.tcrln.mongodb.net/';
+
+async function run() {
+  try {
+    await mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+    console.log('Connected to MongoDB!');
+  } catch (error) {
+    console.error('Failed to connect to MongoDB:', error);
+  }
+}
+
+run().catch(console.dir);
+
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:false}))
-app.use((req,res,next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*')
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization')
-    res.setHeader("Access-Control-Allow-Method","GET,POST,PATCH,DELETE,OPTIONS,PUT")
-    next();
-})
+app.use(bodyParser.urlencoded({ extended: false }));
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE, OPTIONS, PUT');
+  next();
+});
 
-// OPTIONS route handler
-app.options('/api/posts/:id', (req, res) => {
-    res.setHeader('Access-Control-Allow-Methods', 'DELETE');
-    res.setHeader('Access-Control-Allow-Methods', 'PUT');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-    res.setHeader('Access-Control-Max-Age', '86400'); // 24 hours
-    res.sendStatus(200);
-  });
-
-app.use("/api/posts",postRoute);
-app.use("/api/user", userRoute)
+app.use('/api/posts', postRoute);
+app.use('/api/user', userRoute);
 app.use(authMiddleware);
+
 module.exports = app;
